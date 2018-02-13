8a00f37f2a52b41d99fb78e5d2196be182ee635a
RbvAv <actingasaparticle@gmail.com>
2018-02-13 20:26:38 +0100

    Implementación layout

--- app/src/main/java/com/grupo5/todo_ocio/Lista.java
+++ app/src/main/java/com/grupo5/todo_ocio/Lista.java
@@ -2,6 +2,8 @@ package com.grupo5.todo_ocio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Lista extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Lista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+    }
+
+    //menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
