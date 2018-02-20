598e0f7281dbec6f46e2a749fcf185015d1991dd
RbvAv <actingasaparticle@gmail.com>
2018-02-20 21:02:40 +0100

    Cuadro de dialogo y código camara (sin ajustar)

--- app/src/main/java/com/grupo5/todo_ocio/Nuevo.java
+++ app/src/main/java/com/grupo5/todo_ocio/Nuevo.java
@@ -4,12 +4,19 @@ package com.grupo5.todo_ocio;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -20,49 +27,64 @@ import com.google.android.gms.maps.MapView;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+
 public class Nuevo extends Activity {
 
     Button btn_Guardar; //Boton para insertar en BD
     TextView txt_nombreLugar, txt_bio;
-    ImageView img_Foto;
+    ImageButton btn_Foto;
     MapView mpv_Localizacion;
     static RatingBar rtnBar;
     static TextView txtRating;
     RadioGroup id_radioGroup;
     //Para acceder a la BD se crea una instancia de la subclase SQLiteOpenHelper
     final BBDD_Metodos_helper helper = new BBDD_Metodos_helper(this);
+    Button bb;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo);
 
 
-        btn_Guardar = (Button)findViewById(R.id.btn_nGuardar);
-        txt_nombreLugar = (TextView)findViewById(R.id.txt_nNombreLugar);
-        txt_bio = (EditText)findViewById(R.id.txt_nBio);
-        img_Foto = (ImageView)findViewById(R.id.img_nFoto);
-        mpv_Localizacion = (MapView)findViewById(R.id.mvp_mapa);
-        rtnBar = (RatingBar)findViewById(R.id.rtnBar);
-        id_radioGroup = (RadioGroup)findViewById(R.id.id_radioGroup);
-        txtRating = (TextView)findViewById(R.id.rtbText);
+        btn_Guardar = (Button) findViewById(R.id.btn_nGuardar);
+        txt_nombreLugar = (TextView) findViewById(R.id.txt_nNombreLugar);
+        txt_bio = (EditText) findViewById(R.id.txt_nBio);
+        btn_Foto = (ImageButton) findViewById(R.id.img_nFoto);
+        mpv_Localizacion = (MapView) findViewById(R.id.mvp_mapa);
+        rtnBar = (RatingBar) findViewById(R.id.rtnBar);
+        id_radioGroup = (RadioGroup) findViewById(R.id.id_radioGroup);
+        txtRating = (TextView) findViewById(R.id.rtbText);
 
         btn_Guardar.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {insertar(v);}
+            public void onClick(View v) {
+                insertar(v);
+            }
         });
 
-        //Para acceder a la BD se crea una instancia de la subclase SQLiteOpenHelper
+//        btn_Foto.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {diaux();}
+//        });
+
+
+
+    }
+
+    //Para acceder a la BD se crea una instancia de la subclase SQLiteOpenHelper
     //    final BBDD_Metodos_helper helper = new BBDD_Metodos_helper(this);
 
-        //Botón para insertar los datos en la BD
+    //Botón para insertar los datos en la BD
 
-     //   btn_Guardar.setOnClickListener(new View.OnClickListener(){
+    //   btn_Guardar.setOnClickListener(new View.OnClickListener(){
 
-     //       @Override
+    //       @Override
     //        public void onClick(View v) {
-                // Gets the data repository in write mode
-         //       SQLiteDatabase db = helper.getWritableDatabase();
+    // Gets the data repository in write mode
+    //       SQLiteDatabase db = helper.getWritableDatabase();
 
                 /* Se indica la clase donde se ha definido la estructura de la BD + el nombre del campo +
                    donde está la info nueva + más los métodos necesarios
@@ -73,24 +95,23 @@ public class Nuevo extends Activity {
 //                values.put(Estructura_BBDD.bio, txt_bio.getText().toString());
 //                values.put(Estructura_BBDD.puntuacion, rtnBar.getNumStars());
 //                values.put(Estructura_BBDD.tag, id_radioGroup.getCheckedRadioButtonId());
-                //values.put(Estructura_BBDD.imagen, img_Foto.get);
-                //values.put(Estructura_BBDD.latitud, mpv_Localizacion.get);
+    //values.put(Estructura_BBDD.imagen, img_Foto.get);
+    //values.put(Estructura_BBDD.latitud, mpv_Localizacion.get);
 
-                // Insert the new row, returning the primary key value of the new row
-                //Aquí se guarda el valor del id de la nueva fila creada
-         //       long newRowId = db.insert(Estructura_BBDD.TABLE_NAME, null, values);
+    // Insert the new row, returning the primary key value of the new row
+    //Aquí se guarda el valor del id de la nueva fila creada
+    //       long newRowId = db.insert(Estructura_BBDD.TABLE_NAME, null, values);
 
-        //        Toast.makeText(getApplicationContext(), "Registro guardado, id: " +
-        //                newRowId, Toast.LENGTH_LONG).show();
-
-            }
-        //});
-
-   // }
+    //        Toast.makeText(getApplicationContext(), "Registro guardado, id: " +
+    //                newRowId, Toast.LENGTH_LONG).show();
 
 
+    //});
 
-    public void insertar(View v){
+    // }
+
+
+    public void insertar(View v) {
 
         // Gets the data repository in write mode
         SQLiteDatabase db = helper.getWritableDatabase();
@@ -100,7 +121,7 @@ public class Nuevo extends Activity {
                    faltan los datos de mapa, imagen... que hay que averiguar como recogerlos
                  */
         ContentValues values = new ContentValues();
-        values.put(Estructura_BBDD.nombre,txt_nombreLugar.getText().toString());
+        values.put(Estructura_BBDD.nombre, txt_nombreLugar.getText().toString());
         values.put(Estructura_BBDD.bio, txt_bio.getText().toString());
         //values.put(Estructura_BBDD.puntuacion, rtnBar.getNumStars());
         //values.put(Estructura_BBDD.tag, id_radioGroup.getCheckedRadioButtonId());
@@ -125,4 +146,11 @@ public class Nuevo extends Activity {
 
         });
     }
+
+
+//    public static void diaux(){
+//        PhotoDialogFragment dialog = new PhotoDialogFragment();
+//        dialog.setMenuVisibility(true);
+//    }
 }
+
---
+++ app/src/main/java/com/grupo5/todo_ocio/PhotoDialogFragment.java
@@ -0,0 +1,83 @@
+package com.grupo5.todo_ocio;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+
+public class PhotoDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.Description)
+                .setPositiveButton(R.string.Camera, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Tomar foto desde cámara
+                    }
+                })
+                .setNegativeButton(R.string.Gallery, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // tomar foto desde galería
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+
+//    public void obtenerClic(View v) {
+//        RadioButton radbtnCamara = (RadioButton) findViewById(R.id.radbtnCamara);
+//        RadioButton radbtnGaleria = (RadioButton) findViewById(R.id.radbtnGaleria);
+//
+//        Intent intent = null;
+//        int code = 0;
+//        if (radbtnCamara.isChecked()) {
+//            intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//            code = DESDE_CAMARA;
+//        }
+//        if (radbtnGaleria.isChecked()) {
+//            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+//            code = DESDE_GALERIA;
+//        }
+//
+//        startActivityForResult(intent, code);
+//    }
+
+    // Función que se ejecuta cuando concluye el intent en el que se solicita la imagen
+    // ya sea desde la cámara como desde la galería
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        Bitmap imagen = null;
+//
+//        if (requestCode == DESDE_CAMARA) {
+//            imagen = (Bitmap) data.getParcelableExtra("data");
+//        }
+//
+//        if (requestCode == DESDE_GALERIA) {
+//            Uri rutaImagen = data.getData();
+//            try {
+//                imagen = BitmapFactory.decodeStream(new BufferedInputStream(getContentResolver().openInputStream(rutaImagen)));
+//            } catch (FileNotFoundException e) { }
+//        }
+//
+//        ImageView iv = (ImageView) findViewById(R.id.imgView);
+//        iv.setImageBitmap(imagen);
+//    }
+}
